7a61ed2b33c92d1456b85c5168b88dd90c858839
Marina Hoffmann Guimarães <167432472+marinaghoffmann@users.noreply.github.com>
2026-04-13 08:03:22 -0300

    fix main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import produtos
 
 app = FastAPI(
-    title="Sistema de Compras Online",
-    description="API para gerenciamento de pedidos, produtos, consumidores e vendedores.",
+    title="Sistema de Gerenciamento de E-Commerce",
+    description="API para gerenciamento de produtos, vendas e avaliações.",
     version="1.0.0",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(produtos.router)
 
 
 @app.get("/", tags=["Health"])
 def health_check():
     return {"status": "ok", "message": "API rodando com sucesso!"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
